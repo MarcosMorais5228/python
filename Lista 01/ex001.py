2bfee47fa779941747eea6238c562167e092aed1
MarcosMorais5228 <5228.morais@gmail.com>
2025-09-15 14:45:33 -0300

    exercícios

--- Lista 01/ex001.py
+++ Lista 01/ex001.py
@@ -18,14 +18,12 @@ elif(pasteis_competidor3 > pasteis_competidor1) and (pasteis_competidor3 > paste
 
 
 if(nome_competidor1 == 'Lineu') or (nome_competidor2 == 'Lineu') or (nome_competidor3 == 'Lineu'):
-    nome_campeao = 'Lineu comeu um pastel com gosto estranho e usou sua autoridade na vigilancia sanitaria para acabar com a competição, Beiçola tá desolado!'
-    print(nome_campeao)
-elif(nome_competidor1 == 'Floriano') or (nome_competidor2 == 'Floriano') or (nome_competidor3 == 'Floriano') and (nome_campeao != 'Floriano'):
-    print(f'A(O) campeã(o) é {nome_campeao}, com {pasteis_campeao} pastéis consumidos!')
-    print(f'Anos comendo pastel e perdeu justo para {nome_campeao}, lastimável, Sr. Flor!')
-elif(nome_campeao == 'Agostinho') and (pasteis_campeao > 100):
-    print('Acho que o Agostinho deve ter escondido alguns pastéis na calça, pilantra!')
-elif(nome_campeao == 'Agostinho') and (100 > pasteis_campeao > 50):
-    print('Agostinho madrugou no taxi e veio cheio de fome para a competição!')
+    print('Lineu comeu um pastel com gosto estranho e usou sua autoridade na vigilancia sanitaria para acabar com a competição, Beiçola tá desolado!')
 else:
     print(f'A(O) campeã(o) é {nome_campeao}, com {pasteis_campeao} pastéis consumidos!')
+    if((nome_competidor1 == 'Floriano') or (nome_competidor2 == 'Floriano') or (nome_competidor3 == 'Floriano')) and (nome_campeao != 'Floriano'):
+        print(f'Anos comendo pastel e perdeu justo para {nome_campeao}, lastimável, Sr. Flor!')
+    if(nome_campeao == 'Agostinho') and (pasteis_campeao > 100):
+        print('Acho que o Agostinho deve ter escondido alguns pastéis na calça, pilantra!')
+    elif(nome_campeao == 'Agostinho') and (100 > pasteis_campeao > 50):
+        print('Agostinho madrugou no taxi e veio cheio de fome para a competição!')
