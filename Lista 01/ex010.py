559260f73cb4448b8f52a48adc2d78b951e71ec6
MarcosMorais5228 <5228.morais@gmail.com>
2025-09-17 10:24:51 -0300

    exercicios

--- Lista 01/ex010.py
+++ Lista 01/ex010.py
@@ -31,11 +31,10 @@ elif(quantidade_pessoas == 1):
         print('Não tem erro, né? Estar no MacLaren’s é como estar em casa.')
     elif(lugar != 'MacLaren’s Pub') and ('Barney' != nome) and ('Marshall' != nome) and ('Robin' != nome) and ('Lily' != nome):
         print('Um lugar habitual, mas com uma galera diferente. Será estranho, mas Ted vai.')
-    print('Saideira de um pra um. Nada melhor do que uma pessoa pra ouvir seus problemas.')
     print('\nRelatório da situação de Ted:')
     print(f'- Ted foi consolado por: {nome}.')
     print(f'- O local de afogar as mágoas foi: {lugar}.')
-    print(f'- {quantidade_pessoas} pessoas se compadeceram com a situação do pobre Ted.')
+    print('- Saideira de um pra um. Nada melhor do que uma pessoa pra ouvir seus problemas.')
     if(lugar == 'MacLaren’s Pub'):
         print(f'- Quantidade de cervejas bebidas pelo grupo: {cerveja} cervejas.')
     print('Pelo visto todo mundo já sabe como funciona a cabeça dele, né? Depois do rolê Ted conseguiu esfriar a cabeça e já tá pronto pra quebrar mais a cara. Quem será que serão os próximos a consolar o querido Ted Mosby?')
@@ -55,29 +54,38 @@ elif(quantidade_pessoas > 1):
         elif(nome[a] == 'Marshall'):
             print('O melhor amigo de Ted com certeza estaria com ele em um momento tão difícil. Marshall realmente sabe o valor da irmandade.')
         elif(nome[a] == 'Lily'):
-            print('Lily adora fazer o papel da irmã mais velha. Com certeza ela irá consolar ele, mas só depois de dizer “Eu avisei”.')    
-    if('Barney' not in nome) and ('Robin' not in nome) and ('Marshall' not in nome) and ('Lily' not in nome):
-        print(f'{nome} vai encarar essa com Ted. Às vezes a gente encontra apoio de quem menos espera.')
+            print('Lily adora fazer o papel da irmã mais velha. Com certeza ela irá consolar ele, mas só depois de dizer “Eu avisei”.')
+        elif(nome[a] != 'Barney') and (nome[a] != 'Lily') and (nome[a] != 'Marshall') and (nome[a] != 'Robin'):
+            print(f'{nome[a]} vai encarar essa com Ted. Às vezes a gente encontra apoio de quem menos espera.')
     if(quantidade_pessoas == 2) and ('Marshall' in nome) and ('Lily' in nome):
         print('Nada melhor que um casal para dar conselhos de relacionamento.')
     elif(quantidade_pessoas == 2) and ('Marshall' in nome) and ('Barney' in nome):
         print('Sem dúvida os melhores amigos de Ted. Mas tomara que Barney não queira implicar com Marshall sobre quem realmente é o melhor amigo dele.')
     elif(quantidade_pessoas == 4) and ('Marshall' in nome) and ('Barney' in nome) and ('Lily' in nome) and ('Robin' in nome):
-        print('O quinteto junto consegue resolver qualquer problema!')
-    elif(quantidade_pessoas == 4) and (('Marshall' not in nome) or ('Barney' not in nome) or ('Lily' not in nome) or ('Robin' not in nome)):
-        print('Saiu um quinteto um pouco diferente do que a gente tá acostumado, mas no fim conseguiram deixar Ted alegre.')
-    
+        print('O quinteto estará reunido nessa jornada! É o momento perfeito pra brincar de “Você conhece o Ted?”.')
+    if('Barney' in nome) and (lugar == 'Arena de Laser Tag'):
+        print('Com certeza a Arena de Laser Tag foi escolhida por influência de Barney. Se arrume Ted, é hora de botar um terno, tomar um whisky e partir pra diversão.')
+    elif(lugar == 'MacLaren’s Pub') and (('Marshall' in nome) or ('Barney' in nome) or ('Lily' in nome) or ('Robin' in nome)):
+        print('Não tem erro, né? Estar no MacLaren’s é como estar em casa.')
+    elif(lugar == 'MacLaren’s Pub') and ('Marshall' not in nome) and ('Barney' not in nome) and ('Lily' not in nome) and ('Robin' not in nome):
+        print('Um lugar habitual, mas com uma galera diferente. Será estranho, mas Ted vai.')
+
     print('\nRelatório da situação de Ted:')
-    if(quantidade_pessoas == 1):
-        print(f'- Ted foi consolado por: {nome}.')
-    elif(quantidade_pessoas == 2):
+    if(quantidade_pessoas == 2):
         print(f'- Ted foi consolado por: {nome[0]} e {nome[1]}.')
     elif(quantidade_pessoas == 3):
         print(f'- Ted foi consolado por: {nome[0]}, {nome[1]} e {nome[2]}.')
     elif(quantidade_pessoas == 4):
         print(f'- Ted foi consolado por: {nome[0]}, {nome[1]}, {nome[2]} e {nome[3]}.')
     print(f'- O local de afogar as mágoas foi: {lugar}.')
-    print(f'- {quantidade_pessoas} pessoas se compadeceram com a situação do pobre Ted.')
+    if(quantidade_pessoas == 2):
+        print('- Duas pessoas se compadeceram com a situação do pobre Ted.')
+    elif(quantidade_pessoas == 3):
+        print('- Três pessoas! Ted conseguiu se divertir bastante.')
+    elif(quantidade_pessoas == 4) and ('Marshall' in nome) and ('Barney' in nome) and ('Lily' in nome) and ('Robin' in nome):
+        print('- O quinteto junto consegue resolver qualquer problema!')
+    elif(quantidade_pessoas == 4) and (('Marshall' not in nome) or ('Barney' not in nome) or ('Lily' not in nome) or ('Robin' not in nome)):
+        print('- Saiu um quinteto um pouco diferente do que a gente tá acostumado, mas no fim conseguiram deixar Ted alegre.')
     if(lugar == 'MacLaren’s Pub'):
         total_cerveja = cerveja*quantidade_pessoas
         print(f'- Quantidade de cervejas bebidas pelo grupo: {total_cerveja} cervejas.')
