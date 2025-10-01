1ce5b608ba8c932f7711349dbca6e1bec8e00bb6
MarcosMorais5228 <5228.morais@gmail.com>
2025-10-01 10:16:37 -0300

    exercicios

--- lista 02/ex010.py
+++ lista 02/ex010.py
@@ -4,38 +4,71 @@ rodadas = int(input())
 
 for i in range(0, rodadas):
     print(f"Rodada {i+1}/{rodadas}:")
+    tentativas = 0
     vidas = 6
-    palavra = input()
-    fantasma = input()
+    palavra = input().lower()
+    fantasma = input().lower()
     comum = ""
         
-    for letra in palavra.lower():
-        if (letra in fantasma.lower()) and (letra not in comum):
+    for letra in palavra:
+        if (letra in fantasma) and (letra not in comum):
             comum = comum + letra
     
     while len(comum) >= 3:
-        print("A palavra fantasma possui 3 letras presentes na palavra secreta. Tente uma com menos de 3 letras iguais.")
+        print(f"A palavra fantasma possui {len(comum)} letras presentes na palavra secreta. Tente uma com menos de 3 letras iguais.")
         comum = ""
-        fantasma = input()
+        fantasma = input().lower()
         
-        for letra in palavra.lower():
-            if (letra in fantasma.lower()) and (letra not in comum):
+        for letra in palavra:
+            if (letra in fantasma) and (letra not in comum):
                 comum = comum + letra
-
-    while (vidas == 6):
-        letra = input()
-        testadas = ""
         
-        if (letra.lower() in testadas.lower()):
-            print(f"A letra '{letra}' já foi testada antes!")
+    testadas = ""
+
+    while (vidas > 0):
+
+        if(len(testadas) == 0):
+            print("Palavra:", end = " ")
+            for i in range(0, len(palavra)):
+                
+                if(i == len(palavra) - 1):
+                    print("_")
+                
+                else:
+                    print("_", end = " ")
+        
+        else:
+            print("=====================")
+            print("Palavra:", end=" ")
+            for l in palavra:
+                if l in testadas:
+                    print(l, end=" ")
+                else:
+                    print("_", end=" ")
+            print(f"\nVidas restantes: {vidas}")
+            print(f"Letras chutadas: {testadas}")
+            print("=====================")
+            
+        letra = input().lower()
+        tentativas += 1
+        
+        if (letra in testadas.lower()):
+            print(f"Você já tentou a letra '{letra}'. Tente outra.")
             
         else:
                 
-            if letra.lower() in palavra.lower():
-                print(f"A letra '{letra}' está no texto.")
+            if (letra in palavra):
+                print(f"Boa! A letra '{letra}' está na palavra.")
                 
-            else:
-                print(f"A letra '{letra}' NÃO está no texto.")
+            elif(letra not in palavra) and (letra not in fantasma):
+                print(f"Naao! A letra '{letra}' não está na palavra. Você perdeu 1 vida.")
+
+                vidas -= 1
+            
+            elif(letra not in palavra) and(letra in fantasma):
+                print(f"CUIDADO! A letra '{letra}' é uma armadilha! Você perdeu 2 vidas.")
+
+                vidas -= 2
                 
             if(testadas == ""):
                 testadas = testadas + letra
@@ -43,5 +76,4 @@ for i in range(0, rodadas):
             else:
                 testadas = testadas + ", " + letra
 
-        print("Letras já testadas:", testadas)
-        continuar = input("Quer testar outra letra? (s/n): ")
+        
