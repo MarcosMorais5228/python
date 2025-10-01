6d1ab7d2ac58b0eda25504efc0533a7183429bc3
MarcosMorais5228 <5228.morais@gmail.com>
2025-10-01 16:11:11 -0300

    exercicios

--- lista 02/ex010.py
+++ lista 02/ex010.py
@@ -28,9 +28,6 @@ for i in range(0, rodadas):
     x = True
     while (vidas > 0) and (x == True):
         x = False
-        letra = input().lower()
-        letras_chutadas += letra
-        tentativas += 1
 
         if(len(testadas) == 0):
             print("Palavra:", end = " ")
@@ -45,52 +42,61 @@ for i in range(0, rodadas):
         else:
             print("=====================")
             print("Palavra:", end=" ")
-            for l in palavra:
-                if l in testadas:
-                    print(l, end=" ")
+            
+            for i in range(0, len(palavra)):
+                l = palavra[i]
+                if (l in testadas):
+                    if(i == len(palavra) - 1):
+                        print(l)
+                    else:
+                        print(l, end=" ")
                 else:
-                    print("_", end=" ")
-            print(f"\nVidas restantes: {vidas}")
+                    if(i == len(palavra) - 1):
+                        print("_")
+                    else:
+                        print("_", end=" ")
+            
+            print(f"Vidas restantes: {vidas}")
             print(f"Letras chutadas: {testadas}")
             print("=====================")
-            
+
+        letra = input().lower()
         
+        if (letra in testadas.lower()):
+            while(letra in testadas.lower()):
+                print(f"Você já tentou a letra '{letra}'. Tente outra.")
+                letra = input().lower() 
+        
+        letras_chutadas += letra 
+
         for i in range(0, len(palavra)):
             if(palavra[i] not in letras_chutadas):
                 x = True
-            
-        if (letra in testadas.lower()):
-                print(f"Você já tentou a letra '{letra}'. Tente outra.")
+        
+        tentativas += 1   
+
+        if (letra in palavra):
+            print(f"Boa! A letra '{letra}' está na palavra.")
+                        
+        elif(letra not in palavra) and (letra not in fantasma):
+            print(f"Naao! A letra '{letra}' não está na palavra. Você perdeu 1 vida.")
+
+            vidas -= 1
                     
+        elif(letra not in palavra) and(letra in fantasma):
+            print(f"CUIDADO! A letra '{letra}' é uma armadilha! Você perdeu 2 vidas.")
+
+            vidas -= 2
+                        
+        if(testadas == ""):
+            testadas = testadas + letra
+                                     
         else:
-                        
-            if (letra in palavra):
-                print(f"Boa! A letra '{letra}' está na palavra.")
-                        
-            elif(letra not in palavra) and (letra not in fantasma):
-                print(f"Naao! A letra '{letra}' não está na palavra. Você perdeu 1 vida.")
-
-                vidas -= 1
-                    
-            elif(letra not in palavra) and(letra in fantasma):
-                print(f"CUIDADO! A letra '{letra}' é uma armadilha! Você perdeu 2 vidas.")
-
-                vidas -= 2
-                        
-            if(testadas == ""):
-                testadas = testadas + letra
-                        
-                        
-            else:
-                testadas = testadas + ", " + letra
-            
-            
-                
-                
+            testadas = testadas + ", " + letra
+                          
     if(vidas <= 0):
         print(f"Fim de jogo! A forca está completa e o Adivinho perdeu. A palavra secreta era: {palavra.capitalize()}.")
 
     elif(x == False):
         print(f"Parabéns, Adivinho! Você descobriu a palavra secreta: {palavra.capitalize()}.")
         print(f"Total de tentativas: {tentativas}")
-        
