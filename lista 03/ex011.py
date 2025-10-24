e6aa5799d950d117e87f4e0d0bb6d0b57ac38739
MarcosMorais5228 <5228.morais@gmail.com>
2025-10-24 13:50:14 -0300

    exercicios

--- lista 03/ex011.py
+++ lista 03/ex011.py
@@ -7,7 +7,7 @@ for i in range(9):
     for j in range(9):
         sudoku[i].append(linha[j])
 
-# transformar em inteiros e marcar quais são fixos
+# transformar em inteiros e marcar os números iniciais
 inicial = []    
 for lin in range(9):
     linha = []
@@ -20,131 +20,73 @@ for lin in range(9):
             linha.append(True)
     inicial.append(linha)
 
+# começar a preencher
 i = 0
 j = 0
 voltando = False
 
 while i < 9:
-    while j < 9:
-        ok = True
-        proximo = True
-        while ok and proximo:
-            if inicial[i][j] == False:
-                proximo = False
-                if sudoku[i][j] < 9 and voltando == False:
-                    sudoku[i][j] = sudoku[i][j] + 1
+    # se for um número inicial, não posso mexer
+    if inicial[i][j] == True:
+        if not voltando:
+            if j == 8:
+                j = 0
+                i += 1
+            else:
+                j += 1
+        else:
+            if j == 0:
+                j = 8
+                i -= 1
+            else:
+                j -= 1
 
-                    # verifica linha
-                    for m in range(9):
-                        if m != j:
-                            if sudoku[i][j] == sudoku[i][m]:
+    # não é inicial            
+    else:
+        sudoku[i][j] = sudoku[i][j] + 1
+
+        # se for maior que 9, tentei todos os números e deu errado
+        if sudoku[i][j] > 9:
+            sudoku[i][j] = 0
+            voltando = True
+            if j == 0:
+                j = 8
+                i -= 1
+            else:
+                j -= 1
+        else:
+            ok = True
+            
+            # checar se o número existe na linha
+            for m in range(9):
+                if m != j and sudoku[i][m] == sudoku[i][j]:
+                    ok = False
+
+            # checar se o número existe na coluna
+            for m in range(9):
+                if m != i and sudoku[m][j] == sudoku[i][j]:
+                    ok = False
+
+            # checar se o número existe no bloco
+            if ok:
+                bloco_i = i // 3
+                bloco_j = j // 3
+                for a in range(bloco_i * 3, bloco_i * 3 + 3):
+                    for b in range(bloco_j * 3, bloco_j * 3 + 3):
+                        if i != a and j != b:
+                            if sudoku[i][j] == sudoku[a][b]:
                                 ok = False
 
-                    # verifica coluna
-                    if ok:
-                        for m in range(9):
-                            if m != i:
-                                if sudoku[i][j] == sudoku[m][j]:
-                                    ok = False
-
-                    # verifica bloco 3x3
-                    if ok:
-                        bloco_i = i // 3
-                        bloco_j = j // 3
-                        for a in range(bloco_i * 3, bloco_i * 3 + 3):
-                            for b in range(bloco_j * 3, bloco_j * 3 + 3):
-                                if i != a or j != b:
-                                    if sudoku[i][j] == sudoku[a][b]:
-                                        ok = False
-
-                    proximo = True
+            # se deu tudo ok, vou pro próximo número
+            if ok:
+                voltando = False
+                if j == 8:
+                    j = 0
+                    i += 1
                 else:
-                    if sudoku[i][j] == 9:
-                        sudoku[i][j] = 0
-                        proximo = False
-                        voltando = True
-                        repete = True
-                        while repete:
-                            if j == 0:
-                                j = 8
-                                i -= 1
-                            else:
-                                j -= 1
-                            if i < 0:
-                                i = 0
-                                j = 0
-                                repete = False
-                            elif inicial[i][j] == False:
-                                repete = False
+                    j += 1
 
-                if ok and proximo:
-                    voltando = False
-                    repete = True
-                    while repete:
-                        if j == 8:
-                            j = 0
-                            i += 1
-                        else:
-                            j += 1
-                        if i >= 9:
-                            repete = False
-                        elif inicial[i][j] == False:
-                            repete = False
-
-                else:
-                    if sudoku[i][j] == 9:
-                        sudoku[i][j] = 0
-                        voltando = True
-                        repete = True
-                        while repete:
-                            if j == 0:
-                                j = 8
-                                i -= 1
-                            else:
-                                j -= 1
-                            if i < 0:
-                                i = 0
-                                j = 0
-                                repete = False
-                            elif inicial[i][j] == False:
-                                repete = False
-                    else:
-                        voltando = False
-                        repete = True
-                        while repete:
-                            if j == 0:
-                                j = 8
-                                i -= 1
-                            else:
-                                j -= 1
-                            if i < 0:
-                                i = 0
-                                j = 0
-                                repete = False
-                            elif inicial[i][j] == False:
-                                repete = False
-            else:
-                if voltando == False:
-                    if j == 8:
-                        j = 0
-                        i += 1
-                    else:
-                        j += 1
-                else:
-                    repete = True
-                    while repete:
-                        if j == 0:
-                            j = 8
-                            i -= 1
-                        else:
-                            j -= 1
-                        if i < 0:
-                            i = 0
-                            j = 0
-                            repete = False
-                        elif inicial[i][j] == False:
-                            repete = False
-
-# imprimir resultado final
+# print final se deu tudo certo
+print('Caso encerrado! Mizael provou sua inocência lógica e o Sudoku foi resolvido!')
 for linha in sudoku:
-    print(' '.join(str(x) for x in linha))
+    print(' '.join(str(numero) for numero in linha))
