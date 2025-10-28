281473dac121a234cb115caca7dffb2ad82d2c04
MarcosMorais5228 <5228.morais@gmail.com>
2025-10-28 00:26:54 -0300

    exercicios

--- lista04/ex004.py
+++ lista04/ex004.py
@@ -1,83 +1,158 @@
-# função calcculadora de dano
-def damage(a, b, c, d):
-    if (c == 'Fogo' and d == 'Grama') or (c == 'Água' and d == 'Fogo') or (c == 'Grama' and d == 'Água') or (c == 'Elétrico' and d == 'Água'):
+# função calculadora de dano
+def damage(ataque, defesa, tipo_atacante, tipo_alvo):
+    # super efetivo
+    if (tipo_atacante == 'fogo' and tipo_alvo == 'grama') or (tipo_atacante == 'agua' and tipo_alvo == 'fogo') or (tipo_atacante == 'grama' and tipo_alvo == 'agua') or (tipo_atacante == 'eletrico' and tipo_alvo == 'agua'):
         mult = 2
-    
-    elif (d == 'Fogo' and c == 'Grama') or (d == 'Água' and c == 'Fogo') or (d == 'Grama' and c == 'Água') or (d == 'Elétrico' and c == 'Água'):
+        proporcao = 'é super efetivo!'
+
+    # não muito efetivo...
+    elif (tipo_alvo == 'fogo' and tipo_atacante == 'grama') or (tipo_alvo == 'agua' and tipo_atacante == 'fogo') or (tipo_alvo == 'grama' and tipo_atacante == 'agua') or (tipo_alvo == 'eletrico' and tipo_atacante == 'agua'):
         mult = 0.5
-    
+        proporcao = 'não é muito efetivo...'
+
     else:
         mult = 1
-    
-    dano = (a - (b / 2)) * mult
-    if dano < 1: dano = 1
+        proporcao = 'N/A'
 
-    return dano
+    dano = (int(ataque) - (int(defesa) / 2)) * mult
+    if dano < 1:
+        dano = 1
 
-def turnos(e, f):
-    t = 1
-    while int(e[2]) > 0 and int(f[2] >0):
-        print(f'-- Turno {t} --\n')
-        
-    
+    return [int(dano), proporcao]
 
-def rodada(g, h):
+# função turnos
+def turnos(equipe, inimigos, ii, jj):
+    turno = 1
+    while int(equipe[ii][2]) > 0 and int(inimigos[jj][2]) > 0:
+        print(f'-- Turno {turno} --\n')
+
+        # equipe ataca primeiro
+        if int(equipe[ii][7]) >= int(inimigos[jj][7]):
+            if int(equipe[ii][2]) > 0:
+                print(f'{equipe[ii][0]} usa {equipe[ii][4]}!')
+                dano = damage(equipe[ii][5], inimigos[jj][3], equipe[ii][1], inimigos[jj][1])
+                inimigos[jj][2] = max(0, inimigos[jj][2] - dano[0])
+                if dano[1] != 'N/A':
+                    print(f'{equipe[ii][4]} {dano[1]}')
+                print(f'Causou {dano[0]} de dano. HP de {inimigos[jj][0]} agora é {inimigos[jj][2]}/{inimigos[jj][8]}.\n')
+
+            if int(inimigos[jj][2]) > 0:
+                print(f'{inimigos[jj][0]} do oponente usa {inimigos[jj][4]}!')
+                dano = damage(inimigos[jj][5], equipe[ii][3], inimigos[jj][1], equipe[ii][1])
+                equipe[ii][2] = max(0, int(equipe[ii][2]) - dano[0])
+                if dano[1] != 'N/A':
+                    print(f'{inimigos[jj][4]} {dano[1]}')
+                print(f'Causou {dano[0]} de dano. HP de {equipe[ii][0]} agora é {equipe[ii][2]}/{equipe[ii][8]}.\n')
+
+        # inimigo ataca primeiro
+        else:
+            if int(inimigos[jj][2]) > 0:
+                print(f'{inimigos[jj][0]} do oponente usa {inimigos[jj][4]}!')
+                dano = damage(inimigos[jj][5], equipe[ii][3], inimigos[jj][1], equipe[ii][1])
+                equipe[ii][2] = max(0, int(equipe[ii][2]) - dano[0])
+                if dano[1] != 'N/A':
+                    print(f'{inimigos[jj][4]} {dano[1]}')
+                print(f'Causou {dano[0]} de dano. HP de {equipe[ii][0]} agora é {equipe[ii][2]}/{equipe[ii][8]}.\n')
+
+            if int(equipe[ii][2]) > 0:
+                print(f'{equipe[ii][0]} usa {equipe[ii][4]}!')
+                dano = damage(equipe[ii][5], inimigos[jj][3], equipe[ii][1], inimigos[jj][1])
+                inimigos[jj][2] = max(0, int(inimigos[jj][2]) - dano[0])
+                if dano[1] != 'N/A':
+                    print(f'{equipe[ii][4]} {dano[1]}')
+                print(f'Causou {dano[0]} de dano. HP de {inimigos[jj][0]} agora é {inimigos[jj][2]}/{inimigos[jj][8]}.\n')
+
+        # mensagem derrota
+        if int(equipe[ii][2]) == 0:
+            print(f'{equipe[ii][0]} foi derrotado!\n')
+        if int(inimigos[jj][2]) == 0:
+            print(f'{inimigos[jj][0]} do oponente foi derrotado!\n')
+
+        turno += 1
+
+# função rodadas
+def rodada(equipe, inimigos):
+    i = 0
+    j = 0
     num = 1
-    print(f'--- Rodada {num} ---')
-    print(f'{g[0]}, eu escolho você!')
-    print(f'{h[0]}, vai!')
-    print('--------------------\n')
-    if int(g[7]) > int(h[7]):
-        turnos(g, h)
+    vitorias_jogador = 0
+    vitorias_inimigo = 0
+
+    while i < len(equipe) and j < len(inimigos):
+        print(f'--- Rodada {num} ---')
+        print(f'{equipe[i][0]}, eu escolho você!')
+        print(f'{inimigos[j][0]}, vai!')
+        print('--------------------\n')
+
+        turnos(equipe, inimigos, i, j)
+
+        # placar
+        if int(equipe[i][2]) == 0:
+            i += 1
+            vitorias_inimigo += 1
+        if int(inimigos[j][2]) == 0:
+            j += 1
+            vitorias_jogador += 1
+
+        num += 1
+        print('--------------------\n')
+        print(f'Placar: {vitorias_jogador} X {vitorias_inimigo}\n')
+
+    # resultado
+    print('========================================')
+    if vitorias_jogador > vitorias_inimigo:
+        print('Parabéns! Você venceu a batalha Pokémon!')
     else:
-        turnos(h, g)
-    print('--------------------\n')
+        print('Que pena! Você foi derrotado.')
+    print('========================================')
 
 
-
-    
-
+# pokémons equipe
 pokemons = []
-
-# receber pokémons
-for i in range(4):
+print('Hora de montar seu time Pokémon!\n')
+for k in range(4):
     pok = input().split(' - ')
+    # criei mais um HP pra servir de HP máximo nas impressões de vida
+    pok.append(pok[2])
     pokemons.append(pok)
 
-# esolher oponente 
+# oponente
+print('Qual membro da Elite Four você deseja enfrentar?\n')
 oponente = input()
 
-# definir pokémons do oponente
+# pokémons oponente
 if oponente == 'lorelei':
     pokemons_inimigo = [
-    ["Lapras", "agua", 220, 50, "Raio de Gelo", 60, "agua", 60],
-    ["Blastoise", "agua", 180, 55, "Hidro Bomba", 65, "agua", 78],
-    ["Victreebel", "grama", 160, 40, "Folha Navalha", 55, "grama", 70],
-    ["Ninetales", "fogo", 170, 45, "Lança-chamas", 60, "fogo", 100]
-]
+        ["Lapras", "agua", 220, 50, "Raio de Gelo", 60, "agua", 60, 220],
+        ["Blastoise", "agua", 180, 55, "Hidro Bomba", 65, "agua", 78, 180],
+        ["Victreebel", "grama", 160, 40, "Folha Navalha", 55, "grama", 70, 160],
+        ["Ninetales", "fogo", 170, 45, "Lança-chamas", 60, "fogo", 100, 170]
+    ]
 elif oponente == 'bruno':
     pokemons_inimigo = [
-    ["Charizard", "fogo", 190, 40, "Presa de Fogo", 70, "fogo", 100],
-    ["Arcanine", "fogo", 180, 50, "Velocidade Extrema", 60, "fogo", 95],
-    ["Kingler", "agua", 170, 60, "Caranguejo Martelo", 65, "agua", 75],
-    ["Jolteon", "eletrico", 150, 35, "Choque do Trovão", 55, "eletrico", 130]
-]
+        ["Charizard", "fogo", 190, 40, "Presa de Fogo", 70, "fogo", 100, 190],
+        ["Arcanine", "fogo", 180, 50, "Velocidade Extrema", 60, "fogo", 95, 180],
+        ["Kingler", "agua", 170, 60, "Caranguejo Martelo", 65, "agua", 75, 170],
+        ["Jolteon", "eletrico", 150, 35, "Choque do Trovão", 55, "eletrico", 130, 150]
+    ]
 elif oponente == 'agatha':
     pokemons_inimigo = [
-    ["Venusaur", "grama", 180, 50, "Raio Solar", 70, "grama", 80],
-    ["Vileplume", "grama", 160, 45, "Pó do Sono", 50, "grama", 50],
-    ["Raichu", "eletrico", 160, 40, "Investida Trovão", 65, "eletrico", 110],
-    ["Poliwrath", "agua", 190, 55, "Soco Dinâmico", 60, "agua", 70]
-]
+        ["Venusaur", "grama", 180, 50, "Raio Solar", 70, "grama", 80, 180],
+        ["Vileplume", "grama", 160, 45, "Pó do Sono", 50, "grama", 50, 160],
+        ["Raichu", "eletrico", 160, 40, "Investida Trovão", 65, "eletrico", 110, 160],
+        ["Poliwrath", "agua", 190, 55, "Soco Dinâmico", 60, "agua", 70, 190]
+    ]
 elif oponente == 'lance':
     pokemons_inimigo = [
-    ["Electabuzz", "eletrico", 180, 45, "Soco de Trovão", 75, "eletrico", 105],
-    ["Jolteon", "eletrico", 170, 35, "Onda de Trovão", 60, "eletrico", 130],
-    ["Exeggutor", "grama", 160, 40, "Bomba de Semente", 65, "grama", 55],
-    ["Magmar", "fogo", 175, 40, "Giro de Fogo", 55, "fogo", 93]
-]
-    
+        ["Electabuzz", "eletrico", 180, 45, "Soco de Trovão", 75, "eletrico", 105, 180],
+        ["Jolteon", "eletrico", 170, 35, "Onda de Trovão", 60, "eletrico", 130, 170],
+        ["Exeggutor", "grama", 160, 40, "Bomba de Semente", 65, "grama", 55, 160],
+        ["Magmar", "fogo", 175, 40, "Giro de Fogo", 55, "fogo", 93, 175]
+    ]
 
+# início
 print('''====================
 A BATALHA VAI COMEÇAR!
-====================''')
+====================\n''')
+
+rodada(pokemons, pokemons_inimigo)
---
+++ lista04/ex005.py
@@ -0,0 +1,182 @@
+# funcao calculadora de distancia 
+def distancia(y1,x1,y2,x2):
+    if x1 > x2:
+        x3 = x1 - x2
+    else:
+        x3 = x2 - x1
+    
+    if y1 > y2:
+        y3 = y1 - y2
+    else:
+        y3 = y2 - y1
+
+    res = max(x3,y3)
+    return res
+
+# funcao atirar
+def atirar(arma, distancia):
+    if arma == 'Espingarda':
+        if distancia <= 2:
+            dano = 25
+        else:
+            dano = 0
+    
+    elif arma == 'Rifle':
+        if distancia == 3:
+            dano = 15
+        else:
+            dano = 5
+    
+    elif arma == 'Metralhadora':
+        if distancia >= 4:
+            dano = 15
+        else:
+            dano = 0
+
+    return dano
+
+# funcao teleporte
+def teleporte(y, x):
+    for i in range(6):
+        for j in range(6):
+            if i == 0 and j == 0:
+                res1 = distancia(y,x,i,j)
+                casa = [0, 0]
+            
+            else:
+                res2 = distancia(y,x,i,j)
+                if res2 >= res1 and matriz[i][j] != 'I':
+                    res1 = res2
+                    casa = [i, j]
+    
+    return casa
+
+#inicio
+print('Sam: Mas que lugar é esse aqui?')
+print('Dollman: WASD... Num exclusivo de PS5? Ah, fala sério!\n')
+
+sam = 100
+neil = 100
+matriz = []
+posicao_s = []
+posiao_n = []
+qtd = 0
+tel = 0
+danoNeil = 0
+hitsFogo = 0
+armamento = 'Rifle'
+
+# receber matriz
+for i in range(6):
+    entrada = input().split(' ')
+    matriz.append(entrada)
+
+# jogo
+pos = 'P'
+pos1 = 'P'
+movimento = input()
+while sam > 0 and neil > 0:
+    for m in range(6):
+        for n in range(6):
+            if matriz[m][n] == 'S':
+                posicao_s = [int(m), int(n)]
+            if matriz[m][n] == 'N':
+                posiao_n = [int(m), int(n)]
+    
+    # Sam se move
+    if movimento == 'W':
+        matriz[posicao_s[0]][posicao_s[1]] = pos
+        pos = matriz[posicao_s[0]-1][posicao_s[1]]
+        matriz[posicao_s[0]-1][posicao_s[1]] = 'S'
+
+        if pos == 'F':
+            sam -= 5
+            hitsFogo += 1
+        
+    elif movimento == 'A':
+        matriz[posicao_s[0]][posicao_s[1]] = pos
+        pos = matriz[posicao_s[0]][posicao_s[1]-1]
+        matriz[posicao_s[0]][posicao_s[1]-1] = 'S'
+
+        if pos == 'F':
+            sam -= 5
+            hitsFogo += 1
+
+    elif movimento == 'S':
+        matriz[posicao_s[0]][posicao_s[1]] = pos
+        pos = matriz[posicao_s[0]+1][posicao_s[1]]
+        matriz[posicao_s[0]+1][posicao_s[1]] = 'S'
+
+        if pos == 'F':
+            sam -= 5
+            hitsFogo += 1
+
+    elif movimento == 'D':
+        matriz[posicao_s[0]][posicao_s[1]] = pos
+        pos = matriz[posicao_s[0]][posicao_s[1]+1]
+        matriz[posicao_s[0]][posicao_s[1]+1] = 'S'
+
+        if pos == 'F':
+            sam -= 5
+            hitsFogo += 1
+    
+    # Sam troca armamento
+    elif movimento == 'Espingarda':
+        armamento = 'Espingarda'
+        print('Arma trocada para Espingarda.')
+    
+    elif movimento == 'Rifle':
+        armamento = 'Rifle'
+        print('Arma trocada para Rifle.')
+
+    elif movimento == 'Metralhadora':
+        armamento = 'Metralhadora'
+        print('Arma trocada para Metralhadora.')
+
+    # Sam atira
+    elif movimento == 'Atirar':
+        d = distancia(posicao_s[0],posicao_s[1],posiao_n[0],posiao_n[1])
+
+        dano = atirar(armamento, d)
+        neil -= dano
+        if dano > 0:
+            tel += 1
+
+        # teleporte de Neil
+        if tel == 3 and neil > 0:
+            nova = teleporte(posicao_s[0], posicao_s[1])
+
+            matriz[posiao_n[0]][posiao_n[1]] = pos1
+            if matriz[nova[0]][nova[1]] != 'N':
+                pos1 = matriz[nova[0]][nova[1]]
+            matriz[nova[0]][nova[1]] = 'N'
+            for i in range(6):
+                print(' '.join(str(letra) for letra in matriz[i]))
+            tel = 0
+    qtd += 1
+
+    if qtd == 4 and neil > 0:
+        print('>>> Você recebe um disparo de Neil! <<<')
+        sam -= 15
+        danoNeil += 15
+        qtd = 0
+
+        if sam <= 40:
+            print('Dollman: A Fragile comeu todos os criptobiontes da DHV Magalhães... Se curar não é uma opção. Tome cuidado, Sam.')
+    
+    if sam > 0 and neil > 0:
+        movimento = input()
+    
+
+likes = 1000 - (danoNeil * 8) - (hitsFogo * 10)
+
+if sam <= 0:
+    print('''\nMISSÃO FALHOU
+==============
+Sam foi derrotado.
+[Sua alma vaga pela Emenda, buscando reencontrar seu corpo perdido...]''')
+
+elif neil <= 0:
+    print(f'''\nMISSÃO COMPLETA! - Investigue a Anomalia
+========================================
+Likes recebidos: 👍 {likes}''')
