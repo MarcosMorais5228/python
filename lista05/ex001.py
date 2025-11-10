c26c655702cda59b0167792e60b7b6d703db802b
MarcosMorais5228 <5228.morais@gmail.com>
2025-11-10 20:41:33 -0300

    exercicios

--- lista05/ex001.py
+++ lista05/ex001.py
@@ -1,32 +1,55 @@
-# calcular combinações e notas usadas
-def combinacoes(x, y, notasl, qnotasl, i, usar):
+# calcular combinações e guardar todas as notas usadas
+def combinacoes(x, notasl, i, comb1):
     if x == 0:
-        return usar + 1, qnotasl
-    elif x < 0:
-        qnotasl[i] -= 1
-        return 0, qnotasl
-    
-    elif x > 0:
-        qnotasl[i] += 1
-        usar, qnotasl = combinacoes(x-notasl[i], y, notasl, qnotasl, i, usar)
+        lista.append(list(a))
+        return comb1 + 1
         
+    elif x < 0 or i >= 5:
+        return comb1
+    
+    a.append(notasl[i])
+    comb1 = combinacoes(x-notasl[i], notasl, i, comb1)
+    a.pop()
+        
+    comb1 = combinacoes(x, notasl, i+1, comb1)
 
-        if i < 4:
-            usar, qnotasl = combinacoes(x, y, notasl, qnotasl, i+1, usar)
+    return comb1
 
-        return usar, qnotasl
-
+lista = []
+a = []
 notas = [100, 50, 20, 10, 5]
-qnotas = [0, 0, 0, 0, 0]
+qnotas1 = []
+qnotas = []
 # receber o valor da conta
 valor_conta = int(input())
 
 # print inicial
 print(f'Calculando possibilidades para o valor: {valor_conta}')
+
+# iniciar função
 if valor_conta != 0:
-    comb, qnotas = combinacoes(valor_conta, valor_conta, notas, qnotas, 0, 0)
+    comb = combinacoes(valor_conta, notas, 0, 0)
+
+    # transformar a lista e listas em uma lista única
+    for m in range(len(lista)):
+        for n in range(len(lista[m])):
+            qnotas1.append(lista[m][n])
+
+    # guardar quantiade de notas usadas
+    for c in range(5):
+        qnotas.append(qnotas1.count(notas[c]))
 else:
     comb = 0
+    qnotas = [0, 0, 0, 0, 0]
 
-print(comb)
-print(qnotas)
+if comb == 0:
+    print('\nInfelizmente, não há como pagar essa conta com as notas disponíveis.')
+
+elif comb == 1:
+    print('\nEssa foi fácil! Só existe 1 possibilidade de pagar essa conta.')
+
+print(f'\nTotal de possibilidades: {comb}')
+print('\nUso das notas:')
+
+for i in range(5):
+    print(f'R${notas[i]}: usada em {qnotas[i]} combinações')
